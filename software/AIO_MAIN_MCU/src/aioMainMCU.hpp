ba489481cbcd288ff22ed8ecabb8dcd727af7b69
Mihir Savadi <mihirsavadi1@vt.edu>
2021-04-18 12:37:31 -0400

    made class skeleton

--- software/AIO_MAIN_MCU/src/aioMainMCU.hpp
+++ software/AIO_MAIN_MCU/src/aioMainMCU.hpp
@@ -13,15 +13,77 @@
 
 #include <SPI.h>
 #include <SD.h>
-#include <Adafruit_GPS.h>
+// #include <Adafruit_GPS.h> 
 
+/* struct to hold all current sense points */
+struct currentData
+{
+    uint16_t shiftSolenoid;
+    uint16_t GSensor;
+    uint16_t syncSensor;
+    uint16_t EGT;
+    uint16_t injector;
+    uint16_t turboSolenoid;
+    uint16_t LTC;
+    uint16_t fuelPump;
+    uint16_t ignitionCoil;
+    uint16_t fan;
+    uint16_t auxiliaryStage;
+    uint16_t motec;
+    uint16_t acdcConverter;
+    uint16_t battery;
+    uint16_t servo;
+};
+
+/* struct to hold all voltage sense data */
+struct voltageData
+{
+    uint16_t acdcConverter;
+    uint16_t battery;
+};
+
+/* struct to hold other signal inputs */
+struct signalInputs
+{
+    // analog signal
+    uint16_t gearSense;
+    uint16_t throttleSignal;
+    uint16_t brakeSignal;
+
+    // digital signals
+    bool bspdFault;
+    bool killsense; //active low signal 
+    bool dataLogButtonPressed;
+    bool neutralButtonPressed;
+    bool launchButtonPressed;
+    bool sdCardDetected;
+};
+
+/*
+    Main class to abstract away all core functionalities and initializations
+    to make programming in main.cpp clean, easy, and consistent.
+*/
 class aioMainMCU
 {
     public:
+        /* Constructor to initialize all pins and peripherals */
+        aioMainMCU();
 
+        /* read all inputs (analog and digital) and update private fields*/
+        void readInputs();
+
+        /* log all inputs into sd card */
+        void logAllInputs();
 
     private:
+        /* struct to hold sampled current sense data*/
+        currentData current;
 
+        /* struct to hold sampled voltage data */
+        voltageData voltage;
+
+        /* struct to hold other signal input data */
+        signalInputs sigIns;
 };
 
 #endif
--- software/AIO_MAIN_MCU/src/main.cpp
+++ software/AIO_MAIN_MCU/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include "aioMainMCU.hpp"
+
 void setup() {
   
   while(1)
